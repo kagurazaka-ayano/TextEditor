66163e0e128a61ab27a63bece9ff460a5ebf58ab
ayano <kagurazaka-ayano@outlook.com>
2023-12-01 13:18:23 -0500

    initial commit

---
+++ include/TextEditor/KeyDefinition.h
@@ -0,0 +1,16 @@
+/**
+ * @parent include
+ * @file KeyDefinition.h
+ * @author ayano
+ * @date 12/1/23
+ * @brief
+*/
+
+#ifndef TEXTEDITOR_KEYDEFINITION_H
+#define TEXTEDITOR_KEYDEFINITION_H
+
+enum KeyboardButton {
+
+};
+
+#endif //TEXTEDITOR_KEYDEFINITION_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "gainput.h"
+#include "KeyDefinition.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
